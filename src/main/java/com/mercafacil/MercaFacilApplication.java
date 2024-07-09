38c773316e3813072526ee0afa7ec00da401882c
Sergio Aguado Garrido <saguado9@gmail.com>
2024-07-09 15:25:42 +0200

    chore: initial commit

---
+++ src/main/java/com/mercafacil/MercaFacilApplication.java
@@ -0,0 +1,13 @@
+package com.mercafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercaFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercafacil/MercaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
